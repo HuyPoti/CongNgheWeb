5c542e7c139cd46f1c6eee442a9d5cce40a4833f
HuyPoti <huynguyengiapoti@gmail.com>
2026-04-25 13:04:36 +0700

    fix: gitigonre and create  environment exxample

--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://localhost:5000/api',
-  googleClientId: '340257884774-6taenamk1rjdqt4ormpaless19b0dp6j.apps.googleusercontent.com',
-};
